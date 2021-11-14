c770691ab97800a565a1b8921ecd9cdd7b3d8c5c
Ymed <omriymed6@gmail.com>
2021-11-14 06:53:40 +0000

    updated intro video crossfade

---
+++ script learning/Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+    public string LevelName;
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(LevelName);
+    }
+}
