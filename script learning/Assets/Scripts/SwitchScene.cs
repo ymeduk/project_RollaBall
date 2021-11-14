417ed73ec8cc8ac6cfaf6a9c9a4fafca0b6d8b57
Ymed <omriymed6@gmail.com>
2021-11-14 12:04:51 +0000

    updated script time manager switch scene

--- script learning/Assets/Scripts/SwitchScene.cs
+++ script learning/Assets/Scripts/SwitchScene.cs
@@ -5,9 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class SwitchScene : MonoBehaviour
 {
+    public float TransitionTime = 1f;
+    public Animator transition;
     public string LevelName;
     void OnTriggerEnter(Collider other)
     {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex +1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("start");
+
+        yield return new WaitForSeconds(TransitionTime);
+
         SceneManager.LoadScene(LevelName);
     }
 }
